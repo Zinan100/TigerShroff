2299379c272ffd8551d67acd3182ab15a185f96d
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+200920082007@users.noreply.github.com>
2022-05-24 06:53:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b main https://github.com/200920082007/Master.git /Tigerf
-cd /TigerShrf
+git clone -b main https://github.com/200920082007/Master.git /Master
+cd /Master
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
